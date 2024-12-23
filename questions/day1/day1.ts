c2f04de4ae1c09322d3acf7b8516a0d015e4e960
Gal Zohar <galzohar@Gals-MacBook-Pro-2.local>
2024-12-23 03:22:13 +0200

    finished part one of first question

--- questions/day1/day1.ts
+++ questions/day1/day1.ts
@@ -1,3 +1,41 @@
+import { day1Input } from "./day1.input";
+
+const __buildInputLists = () => {
+  return day1Input.split("\n").reduce(
+    (res, line) => {
+      const [firstItem, secondItem] = line.split(/\s+/);
+      return {
+        firstList: [...res.firstList, Number(firstItem)],
+        secondList: [...res.secondList, Number(secondItem)],
+      };
+    },
+    {
+      firstList: [] as number[],
+      secondList: [] as number[],
+    }
+  );
+};
+
+const __readInput = () => {
+  const { firstList, secondList } = __buildInputLists();
+  return {
+    firstList: firstList.sort(),
+    secondList: secondList.sort(),
+  };
+};
+
+const __areListsValid = (first: number[], second: number[]) => {
+  return first.length === second.length;
+};
+
 export const day1 = () => {
-  console.log("hello world from day1");
+  const { firstList, secondList } = __readInput();
+
+  if (!__areListsValid(firstList, secondList)) {
+    throw new Error("Invalid lists. input lengths differ");
+  }
+
+  return firstList.reduce((res, _, index) => {
+    return res + Math.abs(firstList[index] - secondList[index]);
+  }, 0);
 };
